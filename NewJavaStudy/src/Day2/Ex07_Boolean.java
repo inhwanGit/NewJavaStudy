7e2eb57b5f048661a3b3707d1d543c98ecfb95a8
NanNo <inhwanpark19991120@gmail.com>
2024-09-04 00:13:04 +0900

    Booalen

--- NewJavaStudy/src/Day2/Ex07_Boolean.java
+++ NewJavaStudy/src/Day2/Ex07_Boolean.java
@@ -1,7 +1,23 @@
 package Day2;
 
+import java.util.Scanner;
+
 public class Ex07_Boolean {
     public static void main(String[] args) {
+        // boolean
+        // : true(참), false(거짓) 논리값만 가지는 자료형
+        boolean check1 = false;
+        boolean check2 = true;
 
+        Scanner sc = new Scanner(System.in);
+        System.out.print("true or false : ");
+        boolean on = sc.nextBoolean();
+
+        if(on){
+            System.out.println("ont");
+        }
+        else {
+            System.out.println("off");
+        }
     }
 }
