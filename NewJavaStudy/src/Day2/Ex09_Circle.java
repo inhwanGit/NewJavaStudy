68ed51991d6c73309f95cfab8834b725f034aa5c
NanNo <inhwanpark19991120@gmail.com>
2024-09-04 00:20:58 +0900

    Final

--- NewJavaStudy/src/Day2/Ex09_Circle.java
+++ NewJavaStudy/src/Day2/Ex09_Circle.java
@@ -1,7 +1,17 @@
 package Day2;
 
+import java.util.Scanner;
+
 public class Ex09_Circle {
     public static void main(String[] args) {
+        // 상수
+        // - 프로그램 실행 동안 값을 변경하지 않고 사용할 값
+        // * 상수 선언 시, 반드시 값을 초기화해야한다.
+        // * 선언 후에 값을 변경하면 에러가 발생한다.
+        // - 키워드 : final
+        final double PI = 3.14;
+        // PI = 3.14; (에러)
+        Scanner sc = new Scanner(System.in);
 
     }
 }
