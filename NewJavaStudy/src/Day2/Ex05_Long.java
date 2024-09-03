f1705bbc1e11c87698959e10076a8773e799f602
NanNo <inhwanpark19991120@gmail.com>
2024-09-04 00:10:25 +0900

    Booalen

--- NewJavaStudy/src/Day2/Ex05_Long.java
+++ NewJavaStudy/src/Day2/Ex05_Long.java
@@ -1,4 +1,20 @@
 package Day2;
 
 public class Ex05_Long {
+    public static void main(String[] args) {
+        // int (4bytes : 32bits) : 2^32개 약 42억 개
+        // int 수 표현 범위 : -21억...~21억....
+
+        // long (8bytes : 64bits) : 2^64 개
+        long ln1 = 1000;
+        long ln2 = 2100000000;
+
+        // long ln3 = 2200000000; 에러가 나는 이유는 기본적인 정수는 Int타입으로 인식하기 떄문
+        // 해결방법 정수뒤에 l 붙여주기
+        long ln3 = 2200000000l;
+
+        System.out.println(ln1);
+        System.out.println(ln2);
+        System.out.println(ln3);
+    }
 }
---
+++ NewJavaStudy/src/Day2/Ex07_Boolean.java
@@ -0,0 +1,7 @@
+package Day2;
+
+public class Ex07_Boolean {
+    public static void main(String[] args) {
+
+    }
+}
