e30d97a6d7c1e157ce0528a69ffb9f2385f0c96e
NanNo <inhwanpark19991120@gmail.com>
2024-09-04 23:27:45 +0900

    ArithmeticOperator

---
+++ NewJavaStudy/src/Day2/Ex12_ArithmeticOperator.java
@@ -0,0 +1,26 @@
+package Day2;
+
+public class Ex12_ArithmeticOperator {
+    public static void main(String[] args) {
+        // 산술 연산자
+        System.out.println(10 + 20);
+        System.out.println(8 - 5);
+        System.out.println(5 * 4);
+        System.out.println(16 / 2);
+
+        System.out.println(8 % 6);
+        System.out.println(1 % 5);
+        System.out.println(2 % 5);
+        System.out.println(3 % 5);
+        System.out.println(4 % 5);
+        System.out.println(5 % 5);
+
+        System.out.println(6 % 5);
+        System.out.println(7 % 5);
+        System.out.println(8 % 5);
+        System.out.println(9 % 5);
+        System.out.println(10 % 5);
+
+        // 5의 배수만 출력하고 싶을 때 사용하면 좋다. (알고리즘) 5의 배수만 찾으세요 같은 문제! [N % 5 == 0]
+    }
+}
