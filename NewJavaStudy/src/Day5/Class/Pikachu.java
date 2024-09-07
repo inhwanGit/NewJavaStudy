af9337a711714bea3e206c2a5a040de36746809f
NanNo <inhwanpark19991120@gmail.com>
2024-09-07 17:16:51 +0900

    PassingArgument

--- NewJavaStudy/src/Day5/Class/Pikachu.java
+++ NewJavaStudy/src/Day5/Class/Pikachu.java
@@ -32,4 +32,10 @@ public class Pikachu {
     public String bAttack(){
         return "전광석화";
     }
+
+    // toString() : 반환하는 문자열을 객체의 출력으로 대체해주는 메소드
+    @Override
+    public String toString() {
+        return "Pickachu [energy=" + energy + ", type= "+type +"]";
+    }
 }
