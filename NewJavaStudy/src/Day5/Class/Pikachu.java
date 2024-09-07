d56f2bf5f82e24f84752842eae8e7d0d62044448
NanNo <inhwanpark19991120@gmail.com>
2024-09-07 16:14:15 +0900

    class

---
+++ NewJavaStudy/src/Day5/Class/Pikachu.java
@@ -0,0 +1,30 @@
+package Day5.Class;
+
+public class Pikachu {
+    // 클래스 : 객체를 정의하는 설계도
+    // 멤버 : 변수, 메소드
+    // 생성자 : 객체가 생성될 때, 실행되는 메소드
+    //        - 객체의 변수를 초기화하는 역할, * 생성자의 이름은 클래스의 이름과 동일해야한다.
+    // [생성자 정의] - 접근지정자 생성자명(매개변수) {}
+    // [메소드 정의] - 접근지정자 반환타입 메소드명(매개변수){}
+
+    // 변수
+    public int energy;
+    public String type;
+
+    // 생성자
+    // * 기본 생성자는 정의하지 않으면 컴파일러가 자동으로 정의해준다.
+    public Pikachu(int energy, String type) {
+        this.energy = energy;
+        this.type = type;
+    }
+
+    // 메소드
+    public String aAttack(){
+       return "백만볼트";
+    }
+
+    public String bAttack(){
+        return "전광석화";
+    }
+}
