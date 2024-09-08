643b7ebd894924255fe54d737bdcd789491afbf1
NanNo <inhwanpark19991120@gmail.com>
2024-09-08 14:56:03 +0900

    final

---
+++ NewJavaStudy/src/Day7/Final/Final.java
@@ -0,0 +1,9 @@
+package Day7.Final;
+
+public final class Final {
+    public void method(){
+        System.out.println("메소드 출력!");
+    }
+}
+
+// class SubClass extends Final{} //  final로 지정한 클래스는 상속을 할 수 없다.
