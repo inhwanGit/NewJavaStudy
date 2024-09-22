3caec432843a9e4cbbb9a79c8762352a29074c30
NanNo <inhwanpark19991120@gmail.com>
2024-09-22 17:19:12 +0900

    TwoTypeGeneric

--- NewJavaStudy/src/main/java/Day12/Generic/TwoTypeGeneric.java
+++ NewJavaStudy/src/main/java/Day12/Generic/TwoTypeGeneric.java
@@ -28,8 +28,14 @@ public class TwoTypeGeneric {
         keyValue.setKey("Java");
         keyValue.setValue(100);
 
-        KeyValue<String, Integer> keyValue2 = new KeyValue();
+        KeyValue<String, Integer> keyValue2 = new KeyValue(); // 우변의 <> 타입지정은 생략 가능
         keyValue2.setKey("Java");
         keyValue2.setValue(200);
+
+        // K 타입은 지정하고, V 타입은 지정하지 않으려면?
+        // Void : 해당 타입에는 변수를 사용하지 않도록 지정
+        KeyValue<String, Integer> keyValue3 = new KeyValue();
+        keyValue3.setKey("키 만");
+        String key3 = keyValue3.getKey();
     }
 }
